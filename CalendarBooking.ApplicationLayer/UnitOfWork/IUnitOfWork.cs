6aabd2320938dcbb33b28ddaf85c483cc60e21bb
Seayki <dudispower@gmail.com>
2023-04-04 11:15:04 +0200

    aksdpa

---
+++ CalendarBooking.ApplicationLayer/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarBooking.ApplicationLayer.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        void CreateTransaction();
+        void Commit();
+        void Rollback();
+        void Save();
+    }
+}
--- CalendarBooking.InfrastructureLayer/Queries/StudentQueryService.cs
+++ CalendarBooking.InfrastructureLayer/Queries/StudentQueryService.cs
@@ -1,4 +1,5 @@
 ﻿using CalendarBooking.ApplicationLayer.Queries;
+using CalendarBooking.ApplicationLayer.UnitOfWork;
 using CalendarBooking.DomainLayer.Entities;
 using CalendarBooking.InfrastructureLayer.Data;
 using Microsoft.EntityFrameworkCore;
@@ -10,21 +11,22 @@ using System.Threading.Tasks;
 
 namespace CalendarBooking.InfrastructureLayer.Queries
 {
-    public class StudentQueryService : IStudentQueryService
+    public class StudentQueryService : IStudentQueryService 
     {
         private readonly DBContext _dbcontext;
+        private readonly IUnitOfWork<DBContext> _unitOfWork;
 
-        public StudentQueryService(DBContext dBContext)
+        public StudentQueryService(IUnitOfWork<DBContext> unitOfWork) 
         {
-            
+            _unitOfWork = unitOfWork;
+        }
+        public StudentQueryService(DBContext dBContext)
+        {           
             _dbcontext = dBContext;
         }
-
-
         public  async Task<IEnumerable<Student>> GetAll()
         {
             var students = await _dbcontext.Students.ToListAsync();
-
             return students;
         }
 
@@ -32,14 +34,12 @@ namespace CalendarBooking.InfrastructureLayer.Queries
         {
             var student = await _dbcontext.Students.FindAsync(Id);
             return student;
-
-
         }
 
 
-        public async Task<IEnumerable<Booking>> GetBookings(int studentID)
+        public async Task<IEnumerable<Booking>?> GetBookings(int studentId)
         {
-            var studentBookings = await _dbcontext.Bookings.Where(b => b.Student.Id == studentID).ToListAsync();
+            var studentBookings = await _dbcontext.Bookings.Where(b => b.Student.Id == studentId).ToListAsync();
             return studentBookings;
         }
 
